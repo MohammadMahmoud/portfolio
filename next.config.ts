ebfe259642181185e3a2db657e7d15449c14776b
Mohammad Mahmoud <MohammadMahmoud@users.noreply.github.com>
2025-12-07 21:09:02 +0100

    Fix: root deployment for custom domain mmjs.com.de

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,10 @@
 import type { NextConfig } from 'next';
 
-const useBasePath = process.env.NEXTPUBLIC_BASE_PATH;
 const nextConfig: NextConfig = {
   reactCompiler: true,
   output: 'export',
-  basePath: useBasePath ? '/portfolio' : '',
-  assetPrefix: useBasePath ? '/portfolio' : '',
+  basePath: '',
+  assetPrefix: '',
   trailingSlash: true,
   images: { unoptimized: true },
 };
